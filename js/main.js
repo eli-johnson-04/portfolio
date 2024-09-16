303a184cdfaa77c9e756ae6eb447f5942f643338
eli-johnson-04 <ed.j53190@gmail.com>
2024-09-16 11:48:15 -0400

    Added Sphere class, messing around
    I've begun messing around with an actual sphere, time to see where this goes

--- js/main.js
+++ js/main.js
@@ -3,23 +3,39 @@ import { gsap } from 'gsap';
 import * as CANNON from 'cannon-es';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Circle from './circle.js';
+import Sphere from './sphere.js';
 
 // Scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor( 0xe8e8e8, 0);
 document.body.appendChild(renderer.domElement);
 
-// Create circle
-const activityCircle = new Circle();
+// Axes helper
+/* X is red
+   Y is green
+   Z is blue */
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add(axesHelper); 
+
+// Sample circle
+/* const activityCircle = new Circle();
 activityCircle.setPosition(0, 0, 0);
 scene.add(activityCircle.mesh);
+ */
+
+// Sample sphere
+const sampleSphere = new Sphere({ wireframe: true });
+sampleSphere.setPosition(0, 0, 0);
+scene.add(sampleSphere.mesh);
 
 // OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // Enable damping (inertia)
 controls.dampingFactor = 0.25; // Damping factor
+controls.enablePan = false;
 
 // Camera positioning
 camera.position.z = 20;
@@ -28,7 +44,7 @@ function animate() {
     requestAnimationFrame(animate);
 
     // Animate the circles
-    activityCircle.animate();
+    //sampleSphere.animate();
 
     renderer.render(scene, camera);
 }
--- js/sphere.js
+++ js/sphere.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 
-export default class Circle {
-    constructor(radius = 5, segments = 32, color = 0xd9d9d9) {
-        this.geometry = new THREE.CircleGeometry(radius, segments);
-        this.material = new THREE.MeshBasicMaterial({ color:color, wireframe: true });
+export default class Sphere {
+    constructor({ radius = 3, segments = 32, color = 0xd9d9d9, wireframe = false } = {}) {
+        this.geometry = new THREE.SphereGeometry(radius, segments);
+        this.material = new THREE.MeshPhysicalMaterial({ color:color, wireframe: wireframe });
 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
     }
@@ -12,8 +12,10 @@ export default class Circle {
         this.mesh.position.set(x, y, z); // may need to tinker with z-pos when content cards are behind circles
     }
 
+    // Basic animation for testing
     animate() {
         this.mesh.rotation.x += 0.01;
         this.mesh.rotation.y += 0.01;
     }
+
 }
