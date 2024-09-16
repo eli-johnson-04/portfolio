b6b76c73ec63218675420908589755ff0c251fde
Elijah Johnson <ed.j53190@gmail.com>
2024-09-16 15:53:16 -0400

    Movin stuff around

--- js/main.js
+++ js/main.js
@@ -2,14 +2,27 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Sphere from './sphere.js';
 
-// Scene
+// Scene and Camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 20;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor( 0xefefef, 1);
 renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
+
+// OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enablePan = false;
+controls.enableZoom = false;
+
+// Lock orbital controls to not go past XY plane
+controls.minAzimuthAngle = -Math.PI / 2;
+controls.maxAzimuthAngle = Math.PI / 2;
 
 // Lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.075);
@@ -31,34 +44,20 @@ var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 var intersects = [];
 
+// Initial Mouse Position
+mouse.x = -1000;
+mouse.y = -1000;
+
 // Detect mouse movement, change mouse position
 function mouseMove(event) {
     mouse.x = ( event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = - ( event.clientY / window.innerHeight) * 2 + 1;
 }
 
-// Initial Mouse Position
-mouse.x = -1000;
-mouse.y = -1000;
-
 // Sample sphere
 const sampleSphere = new Sphere();
 sampleSphere.setPosition(0, 0, 0);
 scene.add(sampleSphere.mesh);
-
-// OrbitControls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.enablePan = false;
-controls.enableZoom = false;
-
-// Lock orbital controls to not go past XY plane
-controls.minAzimuthAngle = -Math.PI / 2;
-controls.maxAzimuthAngle = Math.PI / 2;
-
-// Camera position
-camera.position.z = 20;
 
 // Handle window resize
 function onWindowResize() {
@@ -68,8 +67,6 @@ function onWindowResize() {
     controls.target.set(0, 0, 0); // Ensure the target is always at the center
     controls.update();
 }
-
-window.addEventListener('resize', onWindowResize, false);
 
 function render() {
     requestAnimationFrame(render);
@@ -97,5 +94,6 @@ function render() {
     renderer.render(scene, camera);
 }
 
+window.addEventListener('resize', onWindowResize, false);
 window.addEventListener('mousemove', mouseMove);
 render();
