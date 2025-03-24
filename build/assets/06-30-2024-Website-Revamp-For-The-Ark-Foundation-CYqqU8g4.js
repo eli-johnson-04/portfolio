299da6b395ab8e05621069ab396eaf1952377866
Elijah Johnson <ed.j53190@gmail.com>
2025-03-24 18:26:24 -0400

    Auto stash before merge of "dev" and "origin/dev"

---
+++ build/assets/06-30-2024-Website-Revamp-For-The-Ark-Foundation-CYqqU8g4.js
@@ -0,0 +1,66 @@
+const e=`# Working in the Gold Coast\r
+In the Summer of 2024 I chose to spend 6 weeks in Ghana, a life-changing experience that provided me with a worldview like no other and a powerful feeling of global citizenship. The University of Florida is pioneering a new service abroad program, where internship work with local nonprofits is incorporated into the study-abroad model. Alongside 17 other students, I built technology for a local organization while taking key courses. I have since resolved to visit all the continents, and I returned with an incomparable sense of intercultural competence. I witnessed the African perspective, saw the impacts of imperialism firsthand, and I will never forget how hot "spicy" can really be. \r
+\r
+![Group Picture at Showcase](/images/pf/showcase-group-pic.jpg)\r
+*My colleagues and I, program supervisor Dr. Sanethia Thomas (front center), and representatives from several of our clients.*\r
+\r
+Still a student, I gained valuable professional and industry experience working directly with The Ark Foundation and leading a team to revitalize their online presence, breathing new life into an 8-year old website. I was blessed with the chance to capitalize on the opportunity to practice the technical skills I had acquired and rigorously put them to use through the software engineering life cycle. \r
+\r
+There were several major components of my time travelling through the country:\r
+- Global citizenry and an individual's impact outside their own culture\r
+- Producing technology to solve problems for local organizations\r
+- Project pitches, update presentations, and a final showcase\r
+- Concurrently taking courses on applications of software engineering and user experience design\r
+- Concentrating on the software development life cycle and Agile methods\r
+- Heavy focus on composing comprehensive project and handoff documentation of current knowledge, assessments and plans, and future goals\r
+- Learning about UXD through the lens of the African perspective\r
+- Serving as a consultant to four other software teams\r
+- Instruction on incorporating AI into my workflow while avoiding parasitism\r
+- Discovering local culture, gaining intercultural competence, and understanding the African perspective\r
+- Exercising open-mindedness, resilience, adaptability, and patience\r
+\r
+## The Ark Foundation\r
+Based in Accra, Ghana, located on the Gold Coast of West Africa, sits the altruistic and faith-based nonprofit Ark Foundation. They provide relief, support, and a wide range of services for survivors of domestic and gender-based abuse, a tragically rampant issue in Ghana. Working with them forced a new understanding of how to communicate with people, even in my own language. Many times I spoke to Ghanaians and both of us had such a thick accent that neither one of us could understand the other. \r
+\r
+Given our limited time in-country, a heavy emphasis of our work was placed on documentation. We recorded many daily scrum sessions, composed extensive documentation on the scope, scale, and motivation of our work, and created a handoff document with every conceivable component of useful information to give to the Ark after we were finished. \r
+\r
+### 8-Year-Old Website\r
+Following our first on-site meeting, the Ark illustrated a desperate need for a new (or at least updated) website. The old one was made somewhat haphazardly, their WordPress systems were 8 years out-of-date, and they had no idea how to do anything related to upkeep or maintenance themselves. My team and I resolved to not only revamp their entire website, but to create an intuitive desktop application that we affectionately named *Noah* for in-house education on website maintenance and management. \r
+\r
+We had many communication headaches due to navigating sociocultural norms and working hours for both parties, but we received permission to overhaul their entire website both technically and design-wise and got to work. \r
+\r
+My team and I were provided access to their cPanel management system and everyone on the Ark's team. We composed and presented the following Project Pitch and carried out our plans using Agile Scrum methodologies for an effective team-based work environment. Do not be mistaken, all three of us were developers despite the title slide!\r
+\r
+<iframe src="https://1drv.ms/b/c/6084949f0d1e5095/IQSVUB4Nn5SEIIBgtO0AAAAAAf37xRYbZszJCx7bOpHhRPU" width="98" height="120" frameborder="0" scrolling="no"></iframe>\r
+\r
+Our plans played out mostly as we had hoped, but the landscape of our work changed frequently. Nonetheless we stayed resilient and adapted to create a website and application we were proud of. Feel free to check it out [here](https://arkfoundationghana.org/site/)! We were thrilled to show it off to everyone we travelled with, our clients at the Ark, and the clients of the other teams at the final showcase, the capstone of our journey. \r
+\r
+![Final Showcase](images/pf/showcase-day.png)\r
+*My team (left-to-right) at the showcase: Pieter, myself, Mouray (student TA, not on-team), and Endrit.*\r
+\r
+\r
+\r
+### Noah Tutorial App\r
+The Noah app was designed to solve a larger issue - yes, the Ark needed a new website, but if they couldn't keep up with it, then a new one would only have the same problem a few years from now. So, I went to Figma and started playing around. Since then, I have taken an entire course on user experience design (also a key component of my work with the Ark Foundation), but this was my first toe-dip. The completed app is faithful to the prototype, and it was here I realized how powerful a tool Figma was. \r
+\r
+<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://embed.figma.com/proto/FmAtjhAgL6TJXNo5EjzTaV/Noah-App-Wireframe?node-id=1-2&p=f&scaling=contain&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2&embed-host=share" allowfullscreen></iframe>\r
+\r
+While my teammates continued work on the website, I got nose-deep in the application. I had heard of Electron and desktop webapps, I had experience with React and Tailwind, and most importantly, I had a mission. I got it all designed and came up with something simple and visually telling, and I built the entire UI and almost the entire application's foundation within a single day (after getting packaging to work :) ). I elected to use JSON as a simple method of writing and creating content, then dynamically creating tutorial cards for each JSON file corresponding to a tutorial. They were split up by folders, and this made it easy to rapid-fire tutorial composition once we tied a bow on the website.\r
+\r
+The *hardest* component of making this application was not my frustration with Tailwind, or JavaScript absurdity, or any other number of petty things. It was easily packaging the app into an executable. It took many hours of tinkering and messing around to get this to work, but nonetheless I succeeded and we released it on [GitHub](https://github.com/eli-johnson-04/noah). We peer-reviewed each other's tutorials, included as many images as we could, worked with the Ark to make sure everything made sense, and we were deliberate with caution and conscientiousness to ensure we didn't just leave them with a tool, but something they could actually rely on. They loved it!\r
+\r
+\r
+# Reflection\r
+The work I did was thoroughly rewarding; I felt like I was actually making a difference. The Ark expressed to us how important their online presence was: it helped draw investors, brought awareness to their services, and provided their basis of communication. There was a persistent thought I had while designing and constructing a modular website structure - we had to not only create something elegant and attractive, but we had to make it simple enough and maintainable so that it could be passed off to a team that informed us of their own lack of tech literacy. \r
+\r
+We succeeded. \r
+\r
+They were thrilled with all our designs - our results were simple and functional, yet responsive and inviting. We conducted think-aloud sessions and our most successful one was with the least tech-literate member on the Ark team. She had only two minor errors that was solely due to a verbose section of text in a Noah tutorial. \r
+\r
+I practiced a variety of hard and soft skills while in the Asante Region. I had just completed my software engineering course and was able to directly employ the software development life cycle and Agile methods. I exercised leadership and project direction with a vision for a better online presence. I got my hands dirty teaching myself a variety of design techniques, web development systems, JavaScript and CSS. Finally, I was blessed to do work with other incredibly talented individuals. On top of all this, my teammates and I served as consultants for designs, programmatic and software engineering questions, SDLC flow issues, and presentation components for four other teams solving technology problems with other companies in Accra. \r
+\r
+I arrived in Ghana with no expectations and I was floored at almost every turn with the richness of culture, of food, of nature, and the kindness of those I worked with and met. I was fortunate to have done, seen, tasted, or experienced just about everything worth visiting the country for, and I would love to go back. \r
+\r
+![Doing the Gator Chomp over a Crocodile LOL](/images/pf/gator-chomp-over-crocodile.jpg)\r
+\r
+*Keywords: software engineering, software development life cycle, Agile methods, Scrum, intercultural competence, African perspective, Electron, ReactJS, NodeJS, TailwindCSS, Git, GitHub, WordPress, cPanel, Gutenberg, Visual Composer, project management, consulting, UXD, Figma, think-aloud*`;export{e as default};
