a893cdbae00565f4efdd1d35898f13ca211c0729
eli <ed.j53190@gmail.com>
2025-03-24 13:06:04 -0400

    changed the name of the production output folder

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './', // Set the root to the current directory
   build: {
-    outDir: 'dist', // Output directory for the build
+    outDir: 'build', // Output directory for the build
   },
   server: {
     open: true, // Open the browser when the server starts
