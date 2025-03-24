58e26dfe7960f5204723b22e025099c111dc230f
eli <ed.j53190@gmail.com>
2025-03-24 14:09:00 -0400

    Fixed JSX to JS conversion, removed unecessary console logs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   root: './', // Set the root to the current directory
   build: {
     outDir: 'build', // Output directory for the build
   },
+  base: '',
   server: {
     open: true, // Open the browser when the server starts
   },
