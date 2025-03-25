409a577475f9f20dab1f11e6fa77a071f609088b
eli <ed.j53190@gmail.com>
2025-03-25 17:13:29 -0400

    Remove unused index.html and update Vite configuration for improved asset handling

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', // Output directory for the build
-    rollupOptions: {
-      input: './index.html',
-      output: {
-        entryFileNames: `assets/[name].[hash].js`, // structure defining how output files are named
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      },
-    },
+    emptyOutDir: true,
   },
+  base: './', // Ensure relative paths for assets
   resolve: {
     extensions: ['.js', '.jsx'], // Ensure .jsx files are resolved
   },
-  base: './', // Ensure relative paths for assets
-  root: './', // Set the root to the current directory
   server: {
     open: true, // Open the browser when the server starts
   },
