c4ccce998dbb2eb4e50df2aebb627d3bb2115143
eli <ed.j53190@gmail.com>
2025-03-27 11:22:16 -0400

    Add development build indicator to popup welcome message

--- js/popup.js
+++ js/popup.js
@@ -22,7 +22,7 @@ export function showPopup() {
     popup.classList.add(...classes);
     popup.innerHTML = `
         <div class="w-full">
-            <h1 class="text-xl font-semibold mb-3 select-none">Welcome!</h1>
+            <h1 class="text-xl font-semibold mb-3 select-none">Welcome! (DEVELOPMENT BUILD)</h1>
             <p class="mb-4 select-none">You can use the mouse and arrow keys to interact with this world.</p>
             <button 
                     class="close-btn text-xl"
