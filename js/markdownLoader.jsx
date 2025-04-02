a4d400004ef72512cf419ecdf99b4947e0e1c3eb
eli <ed.j53190@gmail.com>
2025-04-01 20:40:21 -0400

    Refactor markdownLoader to improve markdown content import structure and enhance loader accessibility

--- js/markdownLoader.jsx
+++ js/markdownLoader.jsx
@@ -8,24 +8,27 @@ export default class markdownLoader {
 
     // Get all markdown content available in website folders. 
     importAllMarkdown() {
-        this.allContent = import.meta.glob('/*/*.md', { query: '?raw', import: 'default' });
+        const rawContent = import.meta.glob('/*/*.md', { query: '?raw', import: 'default' });
+        this.allContent = Object.entries(rawContent).map(([path, loader]) => ({
+            path, // Add the path property
+            loader,
+        }));
         console.log("Finished importing markdown files.");
-        //console.log(this.allContent);
     }
 
     // Helper function that gets a specific set of markdown content. 
     async makeFeed(folder) {
         // Filter out the specified content. 
-        const entries = Object.entries(this.allContent)
-            .filter(([path]) => path.includes(`/${folder}/`))
-            .map(([path, entry]) => {
+        const entries = this.allContent
+            .filter(({ path }) => path.includes(`/${folder}/`))
+            .map(({ path, loader }) => {
                 const id = path.split(`/${folder}/`)[1].replace('.md', '');
-                return { path, id }; 
+                return { path, id, loader }; 
             });
 
         // Resolve the content of all markdown files. 
         const contentPromises = entries.map(async (entry) => {
-            const md = await this.allContent[entry.path](); // Resolves the markdown content
+            const md = await entry.loader(); // Resolves the markdown content
             return { ...entry, md }; // Merge the content with the other data
         });
         
